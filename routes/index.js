e83063343d5133cc4a3410c0fc34ff9a01169025
JonatanCespedes <retratos.jonatan@gmail.com>
2021-02-02 09:55:59 -0300

    Subo los import CSS

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Comisión 4' });
 });
+router.get('/products', function(req, res, next) {
+  res.render('products', { title: 'Comisión 4' });
+});
 
 module.exports = router;
